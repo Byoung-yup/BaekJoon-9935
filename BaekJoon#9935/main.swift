94af96ffc62616277f7b3f0d42bfd09a22a93026
Dev_Ozzy <20170926@vision.hoseo.edu>
2023-04-04 01:41:12 +0900

    commit

--- BaekJoon#9935/main.swift
+++ BaekJoon#9935/main.swift
@@ -7,5 +7,33 @@
 
 import Foundation
 
-print("Hello, World!")
+func solution() {
+    
+    let input = readLine()!.map { String($0) }
+    let target = readLine()!
+    
+    let targetLen = target.count
+    let lastCh = String(target.last!)
+    var stack: [String] = []
 
+    for ch in input {
+
+        stack.append(ch)
+
+        if ch == lastCh {
+
+            let len = stack.count
+            if len >= targetLen && stack[(len - targetLen)...].joined() == target {
+
+                (0..<targetLen).forEach { _ in
+                    stack.removeLast()
+                }
+            }
+        }
+    }
+    
+    print(stack.isEmpty ? "FRULA" : stack.joined())
+    
+}
+
+solution()
